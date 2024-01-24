eae8c986ef8a39675328afaca4e3d2aea4292573
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2024-01-24 23:38:44 +0000

    clean up inline asm header

--- src/asm.h
+++ src/asm.h
@@ -1,10 +1,25 @@
 #ifndef ASM_H
 #define ASM_H
 
-/* INLINE ASSEMBLY HELPERS */
-
 #include "stdint.h"
-extern inline uint8_t inb(uint16_t port);
-extern inline void outb(uint16_t port, uint8_t val);
 
+/*
+    sends 8 bits to an i/o port
+*/
+static inline void outb(uint16_t port, uint8_t val) {
+      asm volatile ( "outb %0, %1" 
+                    : 
+                    : "a"(val), "Nd"(port) );
+}
+
+/*
+    recieves 8 bits from a i/o port
+*/
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile ( "inb %1, %0"
+                : "=a"(ret)
+                : "Nd"(port) );
+    return ret; 
+}
 #endif
