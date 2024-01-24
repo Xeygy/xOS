e75a00c1133a59f43237959722f0bb90f7069eba
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2024-01-24 23:14:58 +0000

    remove printk tests in kmain

--- src/kmain.c
+++ src/kmain.c
@@ -8,39 +8,10 @@
 
 /* kernel main function */
 int kmain() {
-    // int i, j;
-    // char num1, num2;
-    char *st;
     int gdb_loop = 1;
-    /* for (i = 0; i < 5; i++) {
-        for (j = 0; j < 10; j++) {
-            num1 = TO_CHAR(i);
-            num2 = TO_CHAR(j);
-            VGA_display_str("Hello World ");
-            VGA_display_char(num1);
-            VGA_display_char(num2);
-            VGA_display_str("\n");
-        }
-    }*/
-    st = "Hello World!";
-    printk("Hello World! len: %lu, expected: %d, percent %%\n", strlen(st), 12);
-    printk("int min: %d, max: %d\n", INT_MIN, INT_MAX);
-    printk("ld: %ld, qd min: %qd, int breaks: %d\n", LONG_MAX, LLONG_MIN, LONG_MAX);
-    printk("qu: %qu, lu: %lu\n", ULLONG_MAX, ULONG_MAX);
-    printk("qx: %qx, lx: %lx \n", ULLONG_MAX, ULONG_MAX);
-    printk("short min: %hd, max: %hd\n", SHRT_MIN, SHRT_MAX);
-    printk("uint max: %u, overflow (+1): %u \n", UINT_MAX, UINT_MAX + 1);
-    printk("uint hex of 28 is: %x, max: %x \n", 28, UINT_MAX);
-    printk("unsigned char test: %c, emoji: %c \n", '&', '🥝');
-    printk("pointer test: %p, string test: %s \n", &st, st);
-    printk("Error tests %O %i %x % ds %");
-    printk("\nempty len: %lu\n", strlen(st));
-    VGA_set_color(VGA_WHITE, VGA_BLUE);
-    printk("PS/2 stuff:\n");
     //while(gdb_loop);
     while (1) {
         printk("%c", getchar());
     }
-    st = "";
     return 0;
 }
